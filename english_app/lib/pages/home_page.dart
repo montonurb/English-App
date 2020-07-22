06d6c616460b5c2d16d8987f0777e4a1b3059ddd
Bruno Monteiro <brunomoncar007@gmail.com>
2020-07-21 22:45:43 -0300

    alterando aba de vogais para informações

--- english_app/lib/pages/home_page.dart
+++ english_app/lib/pages/home_page.dart
@@ -39,7 +39,7 @@ class _HomePageState extends State<HomePage>
           tabs: <Widget>[
             Tab(text: "Bichos"),
             Tab(text: "Números"),
-            Tab(text: "Informações")
+            Tab(text: "Informações"),
           ],
         ),
       ),
