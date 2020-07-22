834d51fce396fca6a95cdd906a3896ac29194980
Bruno Monteiro <brunomoncar007@gmail.com>
2020-07-21 22:22:44 -0300

    criando layout com imagens e função de executar o som do número clicado

--- english_app/lib/pages/screens/numeros.dart
+++ english_app/lib/pages/screens/numeros.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 class NumerosScreen extends StatefulWidget {
   @override
@@ -6,15 +7,69 @@ class NumerosScreen extends StatefulWidget {
 }
 
 class _NumerosScreenState extends State<NumerosScreen> {
+
+  AudioCache _audioCache = AudioCache(prefix: "audios/");
+
+  _executar(String audioNumero) {
+    _audioCache.play("$audioNumero.mp3");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _audioCache.loadAll([
+      "1.mp3",
+      "2.mp3",
+      "3.mp3",
+      "4.mp3",
+      "5.mp3",
+      "6.mp3",
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Teste Numeros")
-        ],
-      ),
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: [
+        GestureDetector(
+          onTap: () {
+            _executar("1");
+          },
+          child: Image.asset("assets/images/1.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("2");
+          },
+          child: Image.asset("assets/images/2.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("3");
+          },
+          child: Image.asset("assets/images/3.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("4");
+          },
+          child: Image.asset("assets/images/4.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("5");
+          },
+          child: Image.asset("assets/images/5.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("6");
+          },
+          child: Image.asset("assets/images/6.png"),
+        ),
+      ],
     );
   }
 }
